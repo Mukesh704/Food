f38919960fcacffcec084cdfbd17f71c7af9d3a0
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-09-20 12:15:25 +0530

    created food model

---
+++ backend/src/models/food.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    video: {
+        type: String,
+        required: true,
+    },
+    description : {
+        type: String,
+    },
+    foodPartner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'FoodPartner'
+    }
+});
+
+const foodModel = mongoose.mongo.model('Food', foodSchema);
+module.exports = foodModel;
